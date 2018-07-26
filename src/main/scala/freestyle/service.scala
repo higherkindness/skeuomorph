d6de746bfd016213bed2a78cc30f74a761000ac9
Pepe García <pepe@pepegar.com>
2018-07-26 18:07:40 +0200

    create service definition

---
+++ src/main/scala/freestyle/service.scala
@@ -0,0 +1,42 @@
+package skeuomorph
+package freestyle
+
+import turtles._
+import turtles.implicits._
+
+object service {
+
+  import util._
+
+  case class Operation[T](name: String, request: T, response: T)
+
+  // We need to introduce an optimization here.  Transform request & response types of operations into
+  case class Service[T](pkg: String, name: String, declarations: List[T], operations: List[Operation[T]])
+
+  def namedTypes[T](t: T)(implicit T: Birecursive.Aux[T, Schema]): T = t.project match {
+    case Schema.TProduct(name, _) => Schema.TNamedType[T](name).embed
+    case Schema.TSum(name, _)     => Schema.TNamedType[T](name).embed
+    case other                    => other.embed
+  }
+
+  def renderService[T](service: Service[T])(implicit T: Birecursive.Aux[T, Schema]): String = {
+    val printDeclarations = service.declarations.map(_.cata(render)).mkString("\n")
+    val printOperations = service.operations.map { op =>
+      val printRequest  = op.request.transCataT(namedTypes).cata(render)
+      val printResponse = op.response.transCataT(namedTypes).cata(render)
+
+      s"def ${op.name}(req: $printRequest): F[$printResponse]"
+    } mkString ("\n  ")
+    val printService = s"""
+@service trait ${service.name}[F[_]] {
+  $printOperations
+}
+"""
+    s"""
+package ${service.pkg}
+$printDeclarations
+$printService
+"""
+  }
+
+}
