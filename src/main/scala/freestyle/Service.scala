6af75934b41e1993d06733cc0f0d70189b20161d
Pepe García <pepe@pepegar.com>
2018-07-31 09:54:27 +0200

    add some docs to Service class

--- src/main/scala/freestyle/Service.scala
+++ src/main/scala/freestyle/Service.scala
@@ -20,26 +20,38 @@ package freestyle
 import turtles._
 import turtles.implicits._
 
-// We need to introduce an optimization here.  Transform request & response types of operations into
+/**
+ * [[Service]] describes a service representation in freestyle-rpc.
+ *
+ * @see http://frees.io/docs/rpc/idl-generation
+ */
 case class Service[T](pkg: String, name: String, declarations: List[T], operations: List[Service.Operation[T]])
 
 object Service {
 
-  import util._
-
+  /**
+   * Each one of the endpoints of the service
+   */
   case class Operation[T](name: String, request: T, response: T)
 
+  /**
+   * Optimization to kill recursion in nested product/sum types while
+   * rendering.
+   */
   def namedTypes[T](t: T)(implicit T: Birecursive.Aux[T, Schema]): T = t.project match {
     case Schema.TProduct(name, _) => Schema.TNamedType[T](name).embed
     case Schema.TSum(name, _)     => Schema.TNamedType[T](name).embed
     case other                    => other.embed
   }
 
-  def renderService[T](service: Service[T])(implicit T: Birecursive.Aux[T, Schema]): String = {
-    val printDeclarations = service.declarations.map(_.cata(render)).mkString("\n")
+  /**
+   * Render a [[Service]] to its String representation
+   */
+  def render[T](service: Service[T])(implicit T: Birecursive.Aux[T, Schema]): String = {
+    val printDeclarations = service.declarations.map(_.cata(util.render)).mkString("\n")
     val printOperations = service.operations.map { op =>
-      val printRequest  = op.request.transCataT(namedTypes).cata(render)
-      val printResponse = op.response.transCataT(namedTypes).cata(render)
+      val printRequest  = op.request.transCataT(namedTypes).cata(util.render)
+      val printResponse = op.response.transCataT(namedTypes).cata(util.render)
 
       s"def ${op.name}(req: $printRequest): F[$printResponse]"
     } mkString ("\n  ")
