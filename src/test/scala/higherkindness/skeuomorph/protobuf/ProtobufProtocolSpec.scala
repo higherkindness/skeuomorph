c6393efb69cdecd707bed7aedb5d5e00489d0795
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-11-24 18:38:27 +0100

    Discover more errors on protobuf serialization

--- src/test/scala/higherkindness/skeuomorph/protobuf/ProtobufProtocolSpec.scala
+++ src/test/scala/higherkindness/skeuomorph/protobuf/ProtobufProtocolSpec.scala
@@ -28,12 +28,10 @@ import higherkindness.droste.data.Mu._
 
 class ProtobufProtocolSpec extends Specification with ScalaCheck {
 
-  val currentDirectory: String                  = new java.io.File(".").getCanonicalPath
-  val root                                      = "/src/test/scala/higherkindness/skeuomorph/protobuf"
-  val path                                      = currentDirectory + s"$root/service"
-  val importRoot                                = Some(currentDirectory + root)
-  val source                                    = ProtoSource(s"bookService.proto", path, importRoot)
-  val protobufProtocol: Protocol[Mu[ProtobufF]] = parseProto[IO, Mu[ProtobufF]].parse(source).unsafeRunSync()
+  val currentDirectory: String = new java.io.File(".").getCanonicalPath
+  val root                     = "/src/test/scala/higherkindness/skeuomorph/protobuf"
+  val path                     = currentDirectory + s"$root/service"
+  val importRoot               = Some(currentDirectory + root)
 
   implicit val arbCompressionType: Arbitrary[CompressionType] = Arbitrary {
     Gen.oneOf(CompressionType.Identity, CompressionType.Gzip)
@@ -41,12 +39,14 @@ class ProtobufProtocolSpec extends Specification with ScalaCheck {
 
   def is = s2"""
   Protobuf Protocol
-
-  It should be possible to print a protocol from a Proto file. $printProtobufProtocol
-
+  It prints models from a models Proto file. ${printModelProtobufProtocol("book")}
+  It prints a service from a service Proto file. ${printServiceProtobufProtocol("bookService")}
   """
 
-  def printProtobufProtocol = prop { (ct: CompressionType, useIdiom: Boolean) =>
+  def parseProtobufProtocol(fileName: String)(ct: CompressionType, useIdiom: Boolean) = {
+    val source                                    = ProtoSource(s"$fileName.proto", path, importRoot)
+    val protobufProtocol: Protocol[Mu[ProtobufF]] = parseProto[IO, Mu[ProtobufF]].parse(source).unsafeRunSync()
+
     val parseProtocol: Protocol[Mu[ProtobufF]] => higherkindness.skeuomorph.mu.Protocol[Mu[MuF]] = {
       p: Protocol[Mu[ProtobufF]] =>
         higherkindness.skeuomorph.mu.Protocol.fromProtobufProto(ct, useIdiom)(p)
@@ -57,49 +57,86 @@ class ProtobufProtocolSpec extends Specification with ScalaCheck {
         higherkindness.skeuomorph.mu.print.proto.print(p)
     }
 
-    (parseProtocol andThen printProtocol)(protobufProtocol).clean must beEqualTo(expectation(ct, useIdiom).clean)
+    (parseProtocol andThen printProtocol)(protobufProtocol)
   }
 
-  def expectation(compressionType: CompressionType, useIdiomaticEndpoints: Boolean): String = {
+  def printModelProtobufProtocol(fileName: String) = prop { (ct: CompressionType, useIdiom: Boolean) =>
+    val result = parseProtobufProtocol(fileName)(ct, useIdiom)
+    println(s"\n${result}\n")
 
-    val serviceParams: String = "Protobuf" +
-      (if (compressionType == CompressionType.Gzip) ", Gzip" else ", Identity") +
-      (if (useIdiomaticEndpoints) ", namespace = Some(\"com.acme\"), methodNameStyle = Capitalize" else "")
+    result.clean must beEqualTo(modelsExpectation.clean)
+  }
 
-    s"""package com.acme
+  def printServiceProtobufProtocol(fileName: String) = prop { (ct: CompressionType, useIdiom: Boolean) =>
+    val result = parseProtobufProtocol(fileName)(ct, useIdiom)
+
+    result.clean must beEqualTo(serviceExpectation(ct, useIdiom).clean)
+  }
+
+  val modelsExpectation: String =
+    """package com.acme
       |
       |import com.acme.author.Author
       |import com.acme.rating.Rating
       |
       |object book {
       |
-      |@message final case class Book(isbn: Long, title: String, author: List[Option[Author]], binding_type: Option[BindingType], rating: Option[Rating])
-      |@message final case class GetBookRequest(isbn: Long)
-      |@message final case class GetBookViaAuthor(author: Option[Author])
-      |@message final case class BookStore(name: String, books: Map[Long, String], genres: List[Option[Genre]], payment_method: Cop[Long :: Int :: String :: Book :: TNil])
+      |  sealed trait Genre
+      |  object Genre {
+      |    case object UNKNOWN extends Genre
+      |    case object SCIENCE_FICTION extends Genre
+      |    case object POETRY extends Genre
+      |  }
       |
-      |sealed trait Genre
-      |object Genre {
-      |  case object UNKNOWN extends Genre
-      |  case object SCIENCE_FICTION extends Genre
-      |  case object POETRY extends Genre
+      |  sealed trait BindingType
+      |  object BindingType {
+      |    case object HARDCOVER extends BindingType
+      |    case object PAPERBACK extends BindingType
+      |  }
+      |
+      |  @message final case class Book(isbn: Long, title: String, author: List[Author], binding_type: BindingType, rating: Rating)
+      |
+      |  @message final case class BookSeries(isbn: Long)
+      |
+      |  @message final case class Series(code: Long, name: String, numberOfBooks: Int, date: String, books: List[BookSeries])
+      |
+      |  @message final case class GetBookRequest(isbn: Long)
+      |
+      |  @message final case class GetBookViaAuthor(author: Author)
+      |
+      |  @message final case class GetBookSeries(isbn: Long)
+      |
+      |  @message final case class BookStore(name: String, books: Map[Long, String], genres: List[Genre], payment_method: Cop[Long :: Int :: String :: Book :: TNil])
+      |
       |}
-      |
-      |
-      |sealed trait BindingType
-      |object BindingType {
-      |  case object HARDCOVER extends BindingType
-      |  case object PAPERBACK extends BindingType
-      |}
-      |
-      |@service($serviceParams) trait BookService[F[_]] {
-      |  def GetBook(req: GetBookRequest): F[Book]
-      |  def GetBooksViaAuthor(req: GetBookViaAuthor): Stream[F, Book]
-      |  def GetGreatestBook(req: Stream[F, GetBookRequest]): F[Book]
-      |  def GetBooks(req: Stream[F, GetBookRequest]): Stream[F, Book]
-      |}
-      |
-      |}""".stripMargin
+      |""".stripMargin
+
+  def serviceExpectation(compressionType: CompressionType, useIdiomaticEndpoints: Boolean): String = {
+    val params: String =
+      "Protobuf" +
+        (if (compressionType == CompressionType.Gzip) ", Gzip" else ", Identity") +
+        (if (useIdiomaticEndpoints) ", namespace = Some(\"com.acme\"), methodNameStyle = Capitalize" else "")
+
+    s"""package com.acme
+       |
+       |import com.acme.book.Book
+       |import com.acme.book.Series
+       |import com.acme.book.GetBookRequest
+       |import com.acme.book.GetBookViaAuthor
+       |import com.acme.book.GetBookSeries
+       |import com.acme.book.BookStore
+       |
+       |object bookService {
+       |
+       |  @service($params) trait BookService[F[_]] {
+       |    def GetBook(req: GetBookRequest): F[Book]
+       |    def GetBooksSeries(req: GetBookSeries): F[Series]
+       |    def GetBooksViaAuthor(req: GetBookViaAuthor): Stream[F, Book]
+       |    def GetGreatestBook(req: Stream[F, GetBookRequest]): F[Book]
+       |    def GetBooks(req: Stream[F, GetBookRequest]): Stream[F, Book]
+       |  }
+       |
+       |}""".stripMargin
   }
 
   implicit class StringOps(self: String) {
