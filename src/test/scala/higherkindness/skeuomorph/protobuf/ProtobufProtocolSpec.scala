e784b62a9a04c7526d32bc27187f0f22da9db4ec
Chris Birchall <chris.birchall@47deg.com>
2020-01-06 11:22:58 +0000

    Add a field with an Either type to the test
    This is to check that the `knownCoproductTypes` optimization is being
    applied correctly.
    
    Also removed the test for printing a protocol, as I'm about to deprecate
    that feature.

--- src/test/scala/higherkindness/skeuomorph/protobuf/ProtobufProtocolSpec.scala
+++ src/test/scala/higherkindness/skeuomorph/protobuf/ProtobufProtocolSpec.scala
@@ -43,34 +43,8 @@ class ProtobufProtocolSpec extends Specification with ScalaCheck {
   def is = s2"""
   Protobuf Protocol
 
-  It should be possible to print a protocol from a Proto file. $printProtobufProtocol
-
-  It should be possible to generate Scala code for a protocol from a Proto file. $codegenProtobufProtocol
+  It should be possible to generate Scala code for a Mu protocol from a Proto file. $codegenProtobufProtocol
   """
-
-  def printProtobufProtocol = prop { (ct: CompressionType, useIdiom: Boolean) =>
-    val parseProtocol: Protocol[Mu[ProtobufF]] => higherkindness.skeuomorph.mu.Protocol[Mu[MuF]] = {
-      p: Protocol[Mu[ProtobufF]] =>
-        higherkindness.skeuomorph.mu.Protocol.fromProtobufProto(ct, useIdiom)(p)
-    }
-
-    val printProtocol: higherkindness.skeuomorph.mu.Protocol[Mu[MuF]] => String = {
-      p: higherkindness.skeuomorph.mu.Protocol[Mu[MuF]] =>
-        higherkindness.skeuomorph.mu.print.proto.print(p)
-    }
-
-    val actual   = (parseProtocol andThen printProtocol)(protobufProtocol)
-    val expected = expectation(ct, useIdiom)
-
-    (actual.clean must beEqualTo(expected.clean)) :| s"""
-      |Actual output:
-      |$actual
-      |
-      |
-      |Expected output:
-      |$expected"
-      """.stripMargin
-  }
 
   def codegenProtobufProtocol = prop { (ct: CompressionType, useIdiom: Boolean) =>
     val parseProtocol: Protocol[Mu[ProtobufF]] => higherkindness.skeuomorph.mu.Protocol[Mu[MuF]] = {
@@ -135,7 +109,8 @@ class ProtobufProtocolSpec extends Specification with ScalaCheck {
       |  @_root_.pbdirect.pbIndex(1) name: _root_.java.lang.String,
       |  @_root_.pbdirect.pbIndex(2) books: _root_.scala.Predef.Map[_root_.scala.Long, _root_.java.lang.String],
       |  @_root_.pbdirect.pbIndex(3) genres: _root_.scala.List[_root_.com.acme.book.Genre],
-      |  @_root_.pbdirect.pbIndex(4,5,6,7) payment_method: _root_.scala.Option[_root_.shapeless.:+:[_root_.scala.Long, _root_.shapeless.:+:[_root_.scala.Int, _root_.shapeless.:+:[_root_.java.lang.String, _root_.shapeless.:+:[_root_.com.acme.book.Book, _root_.shapeless.CNil]]]]]
+      |  @_root_.pbdirect.pbIndex(4,5,6,7) payment_method: _root_.scala.Option[_root_.shapeless.:+:[_root_.scala.Long, _root_.shapeless.:+:[_root_.scala.Int, _root_.shapeless.:+:[_root_.java.lang.String, _root_.shapeless.:+:[_root_.com.acme.book.Book, _root_.shapeless.CNil]]]]],
+      |  @_root_.pbdirect.pbIndex(8,9) either: _root_.scala.Option[_root_.scala.Either[_root_.scala.Long, _root_.scala.Int]]
       |)
       |
       |sealed abstract class Genre(val value: _root_.scala.Int) extends _root_.enumeratum.values.IntEnumEntry
